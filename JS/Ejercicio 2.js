fa2865af092cc8b098adb3d1de7123ab815b23bd
Rafael <Rafa_sq1999@hotmail.com>
2019-05-16 12:53:24 -0500

    v1 personal

---
+++ JS/Ejercicio 2.js
@@ -0,0 +1,9 @@
+document.getElementById("Uno").innerHTML = "Rafael Sanchez Quirarte";
+
+document.getElementById("Dos").innerHTML = "Pasatiempos:";
+
+document.getElementById("Tres").innerHTML = "1. Salir con amigos";
+
+document.getElementById("Cuatro").innerHTML = "2. Jugar";
+
+document.getElementById("Cinco").innerHTML = "3. Aprender cosas nuevas";
